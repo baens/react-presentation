3239a95b3a061dce7036f3a484fc524c929d3f74
Erik Lindblom <elindblom@overstock.com>
2016-06-15 15:01:55 -0600

    Step 16: Fetch initial data from API server

--- src/server/index.js
+++ src/server/index.js
@@ -4,20 +4,25 @@ import fs from 'fs';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import {ButtonComponent} from '../components/Button';
+import httpRequest from 'request';
 
 const app = express();
 
 app.get('/', (request, response) => {
   const htmlPage = fs.readFileSync(path.join(__dirname, './index.html')).toString();
 
-  const initialData = {
-    count : 4
-  };
+  httpRequest('http://localhost:8081/', (error, httpResponse, body) => {
+    const httpData = JSON.parse(body);
 
-  response.send(htmlPage
-                .replace('__REACT__', ReactDOMServer.renderToString(<ButtonComponent data={initialData}/>))
-                .replace('__DATA__', JSON.stringify(initialData))
-      );
+    const initialData = {
+      count : httpData.number
+    };
+
+    response.send(htmlPage
+        .replace('__REACT__', ReactDOMServer.renderToString(<ButtonComponent data={initialData}/>))
+        .replace('__DATA__', JSON.stringify(initialData))
+    );
+  });
 });
 
 app.get('/client.js', (request, response) => response.sendFile(path.join(__dirname,'../build/client.js')));
