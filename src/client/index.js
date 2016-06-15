7863c3e54d67824ff3dcf635da9482a7f0cc72b5
Erik Lindblom <elindblom@overstock.com>
2016-06-14 21:17:02 -0600

    Step 3: Add event handling

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,16 @@
 var ButtonComponent = React.createClass({
+  getInitialState: function () {
+    return {
+      count : 0
+    };
+  },
+
+  click: function() {
+    this.setState({count: ++this.state.count});
+  },
+
   render: function(){
-    return React.createElement('button',{},'Click me');
+    return React.createElement('button',{onClick:this.click},'Click me '+this.state.count);
   }
 });
 
