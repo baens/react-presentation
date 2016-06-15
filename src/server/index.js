cf01ee0f386d947f4fa61bb01550d6f55de35039
Erik Lindblom <elindblom@overstock.com>
2016-06-15 05:58:06 -0600

    Step 11: Render component on server

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,17 @@
 import path from 'path';
 import express from 'express';
+import fs from 'fs';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import {ButtonComponent} from '../components/Button';
 
 const app = express();
 
-app.get('/', (request, response) => response.sendFile(path.join(__dirname, './index.html')));
+app.get('/', (request, response) => {
+  const htmlPage = fs.readFileSync(path.join(__dirname, './index.html')).toString();
+
+  response.send(htmlPage.replace('REPLACE_ME', ReactDOMServer.renderToString(<ButtonComponent />)));
+});
 
 app.get('/client.js', (request, response) => response.sendFile(path.join(__dirname,'../build/client.js')));
 
