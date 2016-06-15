a8c9d75d5b4c9d8a12c1aaba09ec52e07dd16c3c
Erik Lindblom <elindblom@overstock.com>
2016-06-14 21:55:37 -0600

    Step 10: Server rebuild on ES2015

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,10 @@
-var path = require('path');
-var express = require('express');
+import path from 'path';
+import express from 'express';
 
-var app = express();
+const app = express();
 
-app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, './index.html'));
-});
+app.get('/', (request, response) => response.sendFile(path.join(__dirname, './index.html')));
 
-app.get('/client.js', function(request, response) {
-  response.sendFile(path.join(__dirname,'../build/client.js'));
-});
+app.get('/client.js', (request, response) => response.sendFile(path.join(__dirname,'../build/client.js')));
 
 app.listen(8080);
